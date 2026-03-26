88067dddb48f84f4c9cf39f71337a3d055a88ebb
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-03-26 22:09:59 +0100

    Added additional displays based on game status

--- frontend/src/components/GameComponents/Article.tsx
+++ frontend/src/components/GameComponents/Article.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, type ReactNode } from "react";
 import type { GameStateDto } from "../../models/GameModels";
-import { mocStateWinGame } from "../../mocks/gameMocks";
+import { mockStateAfterWrongGuess1 } from "../../mocks/gameMocks";
 import { CensorIcon } from "../../assets/CensorIcon";
 import { useLoading } from "../../store/LoadingContext";
 
@@ -13,7 +13,7 @@ function Article() {
     setIsLoading(true);
     // mock fetching data
     const timeout = setTimeout(() => {
-      setGameState(mocStateWinGame);
+      setGameState(mockStateAfterWrongGuess1);
 
       setIsLoading(false);
     }, 2000);
@@ -44,10 +44,40 @@ function Article() {
     <div>
       {!isLoading && (
         <div className="flex flex-col items-center">
-          <div className="text-4xl mb-15">
-            Revealed {gameState?.revealedSentences.length} /{" "}
-            {gameState?.totalSentencesNum} sentences
-          </div>
+          {gameState?.gameStatus === "IN_PROGRESS" && (
+            <div className="flex mb-20 text-8xl">
+              <CensorIcon className="w-25 h-25 inline-block" />
+              <span>?</span>
+            </div>
+          )}
+
+          {gameState?.gameStatus !== "IN_PROGRESS" && (
+            <div className="mb-20 text-8xl">{gameState?.articleTitle}</div>
+          )}
+
+          {gameState?.gameStatus === "IN_PROGRESS" && (
+            <div className="text-4xl mb-15">
+              Revealed {gameState?.revealedSentences.length} /{" "}
+              {gameState?.totalSentencesNum} sentences
+            </div>
+          )}
+
+          {gameState?.gameStatus === "LOST" && (
+            <div className="text-4xl mb-15 bg-rose-50 p-5 rounded-xl">
+              Oh no! You lost...
+            </div>
+          )}
+
+          {gameState?.gameStatus === "WON" && (
+            <div className="flex flex-col items-center text-4xl mb-15 gap-5 bg-lime-100 p-5 rounded-xl">
+              <span>Congratulations, you WIN!!!</span>
+              <span>
+                It took you{" "}
+                {gameState?.totalSentencesNum - gameState?.guessesLeftNum}{" "}
+                guesses to get it right :&#x29;
+              </span>
+            </div>
+          )}
 
           <div className="max-w-6xl text-justify text-2xl/15">
             {gameState?.revealedSentences.map((sentence) => (
