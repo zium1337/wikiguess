06e1fea5600b6a1e8bd31f548f2e3f8749f13792
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-03-26 23:13:44 +0100

    Moved the loading and fetching data to the Game page

--- frontend/src/components/GameComponents/Article.tsx
+++ frontend/src/components/GameComponents/Article.tsx
@@ -1,25 +1,13 @@
-import { useEffect, useState, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import type { GameStateDto } from "../../models/GameModels";
-import { mockStateAfterWrongGuess1 } from "../../mocks/gameMocks";
 import { CensorIcon } from "../../assets/CensorIcon";
-import { useLoading } from "../../store/LoadingContext";
 
-function Article() {
+interface ArticleProps {
+  gameState: GameStateDto;
+}
+
+function Article({ gameState }: ArticleProps) {
   const MAREKD_AS_CENSORED = "_censoredWord_";
-  const { isLoading, setIsLoading } = useLoading();
-  const [gameState, setGameState] = useState<GameStateDto>();
-
-  useEffect(() => {
-    setIsLoading(true);
-    // mock fetching data
-    const timeout = setTimeout(() => {
-      setGameState(mockStateAfterWrongGuess1);
-
-      setIsLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timeout);
-  }, []);
 
   const censorSentence = (sentence: string): ReactNode => {
     const wordsInSentence = sentence.split(" ");
@@ -41,51 +29,46 @@ function Article() {
   };
 
   return (
-    <div>
-      {!isLoading && (
-        <div className="flex flex-col items-center">
-          {gameState?.gameStatus === "IN_PROGRESS" && (
-            <div className="flex mb-20 text-8xl">
-              <CensorIcon className="w-25 h-25 inline-block" />
-              <span>?</span>
-            </div>
-          )}
-
-          {gameState?.gameStatus !== "IN_PROGRESS" && (
-            <div className="mb-20 text-8xl">{gameState?.articleTitle}</div>
-          )}
-
-          {gameState?.gameStatus === "IN_PROGRESS" && (
-            <div className="text-4xl mb-15">
-              Revealed {gameState?.revealedSentences.length} /{" "}
-              {gameState?.totalSentencesNum} sentences
-            </div>
-          )}
-
-          {gameState?.gameStatus === "LOST" && (
-            <div className="text-4xl mb-15 bg-rose-50 p-5 rounded-xl">
-              Oh no! You lost...
-            </div>
-          )}
-
-          {gameState?.gameStatus === "WON" && (
-            <div className="flex flex-col items-center text-4xl mb-15 gap-5 bg-lime-100 p-5 rounded-xl">
-              <span>Congratulations, you WIN!!!</span>
-              <span>
-                It took you{" "}
-                {gameState?.totalSentencesNum - gameState?.guessesLeftNum}{" "}
-                guesses to get it right :&#x29;
-              </span>
-            </div>
-          )}
-
-          <div className="max-w-6xl text-justify text-2xl/15">
-            {gameState?.revealedSentences.map((sentence) => (
-              <div className="mb-10">{censorSentence(sentence.text)}</div>
-            ))}
-          </div>
+    <div className="flex flex-col items-center">
+      {gameState.gameStatus === "IN_PROGRESS" && (
+        <div className="flex mb-20 text-8xl">
+          <CensorIcon className="w-25 h-25 inline-block" />
+          <span></span>
         </div>
       )}
+
+      {gameState.gameStatus !== "IN_PROGRESS" && (
+        <div className="mb-20 text-8xl">{gameState.articleTitle}</div>
+      )}
+
+      {gameState.gameStatus === "IN_PROGRESS" && (
+        <div className="text-4xl mb-15">
+          Revealed {gameState.revealedSentences.length} /{" "}
+          {gameState.totalSentencesNum} sentences
+        </div>
+      )}
+
+      {gameState.gameStatus === "LOST" && (
+        <div className="text-4xl mb-15 bg-rose-50 p-5 rounded-xl">
+          Oh no! You lost...
+        </div>
+      )}
+
+      {gameState.gameStatus === "WON" && (
+        <div className="flex flex-col items-center text-4xl mb-15 gap-5 bg-lime-100 p-5 rounded-xl">
+          <span>Congratulations, you WIN!!!</span>
+          <span>
+            It took you {gameState.totalSentencesNum - gameState.guessesLeftNum}{" "}
+            guesses to get it right :&#x29;
+          </span>
+        </div>
+      )}
+
+      <div className="max-w-6xl text-justify text-2xl/15">
+        {gameState.revealedSentences.map((sentence) => (
+          <div className="mb-10">{censorSentence(sentence.text)}</div>
+        ))}
+      </div>
     </div>
   );
 }
