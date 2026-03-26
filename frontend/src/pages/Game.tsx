69f48519e75acd83e7ad6de727bb7537d71484fd
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-03-26 22:58:44 +0100

    Added user input component

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
 import Article from "../components/GameComponents/Article";
+import InputField from "../components/InputField";
 
 function Game() {
+  const [guess, setGuess] = useState<string>("");
   return (
     <div className="flex flex-col justify-center items-center">
+      <InputField
+        placeholderText="Type in your guess..."
+        userInput={guess}
+        setUserInput={setGuess}
+        width={1100}
+        height={100}
+      />
       <Article />
     </div>
   );
