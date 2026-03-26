a7d0d08b8bf3b3989d163653d1f7053bb857519b
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-03-26 21:14:47 +0100

    Added articles sentences display

---
+++ frontend/src/components/GameComponents/Article.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState, type ReactNode } from "react";
+import LoadingOverlay from "../LoadingOverlay";
+import type { GameStateDto } from "../../models/GameModels";
+import { mockStateAfterWrongGuess2 } from "../../mocks/gameMocks";
+import { CensorIcon } from "../../assets/CensorIcon";
+
+function Article() {
+  const MAREKD_AS_CENSORED = "_censoredWord_";
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [gameState, setGameState] = useState<GameStateDto>();
+
+  useEffect(() => {
+    setIsLoading(true);
+    // mock fetching data
+    const timeout = setTimeout(() => {
+      setGameState(mockStateAfterWrongGuess2);
+
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const censorSentence = (sentence: string): ReactNode => {
+    const wordsInSentence = sentence.split(" ");
+
+    return wordsInSentence.map((word, index) => {
+      const shouldBeCensored = word === MAREKD_AS_CENSORED;
+
+      return (
+        <span key={index}>
+          {shouldBeCensored ? (
+            <CensorIcon className="inline-block w-10 h-10 align-middle mx-1" />
+          ) : (
+            word
+          )}
+          {index < wordsInSentence.length - 1 && " "}
+        </span>
+      );
+    });
+  };
+
+  return (
+    <div>
+      {isLoading && <LoadingOverlay />}
+
+      {!isLoading && (
+        <div className="flex flex-col items-center">
+          <div className="text-4xl mb-15">
+            Revealed {gameState?.revealedSentences.length} /{" "}
+            {gameState?.totalSentencesNum} sentences
+          </div>
+
+          <div className="max-w-6xl text-justify text-2xl/15">
+            {gameState?.revealedSentences.map((sentence) => (
+              <div className="mb-10">{censorSentence(sentence.text)}</div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+export default Article;
