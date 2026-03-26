089eb675d1e57399639c210ae57fcaeb02e983b5
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-03-26 23:13:44 +0100

    Moved the loading and fetching data to the Game page

--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,19 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Article from "../components/GameComponents/Article";
 import InputField from "../components/InputField";
+import { useLoading } from "../store/LoadingContext";
+import { mockStateAfterWrongGuess1 } from "../mocks/gameMocks";
+import type { GameStateDto } from "../models/GameModels";
 
 function Game() {
+  const { isLoading, setIsLoading } = useLoading();
+  const [gameState, setGameState] = useState<GameStateDto>();
   const [guess, setGuess] = useState<string>("");
+
+  useEffect(() => {
+    setIsLoading(true);
+    // mock fetching data
+    const timeout = setTimeout(() => {
+      setGameState(mockStateAfterWrongGuess1);
+
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
-    <div className="flex flex-col justify-center items-center">
-      <InputField
-        placeholderText="Type in your guess..."
-        userInput={guess}
-        setUserInput={setGuess}
-        width={1100}
-        height={100}
-      />
-      <Article />
+    <div>
+      {!isLoading && gameState !== undefined && (
+        <div className="flex flex-col justify-center items-center">
+          <InputField
+            placeholderText="Type in your guess..."
+            userInput={guess}
+            setUserInput={setGuess}
+            width={1100}
+            height={100}
+          />
+          <Article gameState={gameState} />
+        </div>
+      )}
     </div>
   );
 }
