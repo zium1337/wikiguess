65bd1dfb555927f0906de86c0f1a8226f9728591
Maja Furmaniuk <ma.furmaniuk@gmail.com>
2026-05-20 21:26:35 +0200

    Corrected the sentences display

--- frontend/src/components/GameComponents/Article.tsx
+++ frontend/src/components/GameComponents/Article.tsx
@@ -6,26 +6,26 @@ interface ArticleProps {
   gameState: GameStateDto;
 }
 
+const NUM_GUESSES = 5;
+
 function Article({ gameState }: ArticleProps) {
   const MARKED_AS_CENSORED = "_censoredWord_";
+  const isGameOver = gameState.gameStatus !== "IN_PROGRESS";
 
-  const censorSentence = (sentence: string): ReactNode => {
-    const wordsInSentence = sentence.split(" ");
+  const renderSentence = (sentence: string): ReactNode => {
+    if (isGameOver && gameState.articleTitle) {
+      return sentence.replaceAll(MARKED_AS_CENSORED, gameState.articleTitle);
+    }
 
-    return wordsInSentence.map((word, index) => {
-      const shouldBeCensored = word === MARKED_AS_CENSORED;
-
-      return (
-        <span key={index}>
-          {shouldBeCensored ? (
-            <CensorIcon className="inline-block w-10 h-10 align-middle mx-1" />
-          ) : (
-            word
-          )}
-          {index < wordsInSentence.length - 1 && " "}
-        </span>
-      );
-    });
+    const parts = sentence.split(MARKED_AS_CENSORED);
+    return parts.map((part, index) => (
+      <span key={index}>
+        {part}
+        {index < parts.length - 1 && (
+          <CensorIcon className="inline-block w-10 h-10 align-middle mx-1" />
+        )}
+      </span>
+    ));
   };
 
   return (
@@ -43,8 +43,8 @@ function Article({ gameState }: ArticleProps) {
 
       {gameState.gameStatus === "IN_PROGRESS" && (
         <div className="text-4xl mb-15">
-          Revealed {gameState.revealedSentences.length} /{" "}
-          {gameState.totalSentencesNum} sentences
+          Revealed {gameState.revealedSentences.length} / {NUM_GUESSES}{" "}
+          sentences
         </div>
       )}
 
@@ -58,15 +58,15 @@ function Article({ gameState }: ArticleProps) {
         <div className="flex flex-col items-center text-4xl mb-15 gap-5 bg-lime-100 p-5 rounded-xl">
           <span>Congratulations, you WIN!!!</span>
           <span>
-            It took you {gameState.totalSentencesNum - gameState.guessesLeftNum}{" "}
-            guesses to get it right :&#x29;
+            It took you {NUM_GUESSES - 1 - gameState.guessesLeftNum} guesses to
+            get it right :&#x29;
           </span>
         </div>
       )}
 
       <div className="max-w-6xl text-justify text-2xl/15">
         {gameState.revealedSentences.map((sentence) => (
-          <div className="mb-10">{censorSentence(sentence.text)}</div>
+          <div className="mb-10">{renderSentence(sentence.text)}</div>
         ))}
       </div>
     </div>
